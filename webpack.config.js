16b93f6627797c49d9e8bcf01fc2723b3ce1a176
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-17 16:17:52 -0500

    usando hashes en los archivos de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,47 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path') 
-module.exports = {
-  // entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              '@babel/preset-react',
-              {
-                runtime: 'automatic' // 'classic'
-              }
-
+module.exports = (env, argv) => {
+  const {mode} = argv 
+  const isProduction = mode === 'production'
+  return {
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : 'main.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic' // 'classic'
+                }
+  
+              ]
             ]
-          ]
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: 'src/index.html'})
-  ],
-  devServer: {
-    open: true, // para que se abra en el navegador
-    port: 3000,
-    compress: true,
-    client: {overlay: true }// para abrir un overlay con los errores
-  },
-  devtool: 'source-map'
-}
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: 'src/index.html'})
+    ],
+    devServer: {
+      open: true, // para que se abra en el navegador
+      port: 3000,
+      compress: true,
+      client: {overlay: true }// para abrir un overlay con los errores
+    },
+    devtool: 'source-map'
+  }
+  }
+  // entry: './src/index.js',
+  
