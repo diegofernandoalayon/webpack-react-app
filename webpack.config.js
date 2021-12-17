6795f865cdae3141da3493ee513067e992314bd5
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-17 15:18:58 -0500

    descargando, configurando y usando plugin de webpack para el html

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path') 
 module.exports = {
   // entry: './src/index.js',
@@ -26,5 +27,8 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: 'src/index.html'})
+  ]
 }
