0d2e9a51b16bcb2f55cc8f79dfd52a1976a3e6e0
Diego Fernando Alayon <dfar.41@gmail.com>
2021-12-17 16:21:00 -0500

    corrigiendo el error gracias a source map

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
 
 
   const [counter, setCounter] = useState(0)
-  const [values, setValues] = useState()
+  const [values, setValues] = useState([])
   const handleClick = () => {
     setCounter(counter + 1)
     setValues(values.concat(counter))
